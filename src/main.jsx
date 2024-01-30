bc35a54599cf4b6e928647ea9550b424d7283b45
Andrii Shevchuk <supamercron@gmail.com>
2024-01-29 20:43:08 -0500

    add routing, add MainLayout

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { MainLayout } from './layouts/MainLayout';
+import './assets/styles/index.scss';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        path: "/",
+        element: <div>Home</div>,
+      },
+      {
+        path: "/pizzas",
+        element: <div>Pizzas</div>,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router} />
 )
