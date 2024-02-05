1189517afbc780d6a860ce2035fae33ae7d8aa54
Andrii Shevchuk <supamercron@gmail.com>
2024-02-05 12:05:16 -0500

    add redux, rtkApi, mainPage

--- src/pages/MainPage/ui/MainPage.jsx
+++ src/pages/MainPage/ui/MainPage.jsx
@@ -1,7 +1,35 @@
+import { useGetOthers, useGetPizzas, useGetRolls } from "@/api/rtkApi";
+import { ProductTape } from "@/components/ProductTape";
+
 const MainPage = () => {
+
+    const {data: productPizzas, isLoading: pizzasLoading, error: pizzasError} = useGetPizzas();
+    const {data: productRolls, isLoading: rollsLoading, error: rollsError} = useGetRolls();
+    const {data: productOthers, isLoading: othersLoading, error: othersError} = useGetOthers();
+
     return (
-        <p>MainPage</p>
-    );
+        <>
+            <ProductTape 
+                title="Пиццы" 
+                products={productPizzas} 
+                isLoading={pizzasLoading} 
+                error={pizzasError} 
+            />
+            <ProductTape 
+                title="Роллы" 
+                products={productRolls} 
+                isLoading={rollsLoading} 
+                error={rollsError} 
+            />
+            <ProductTape 
+                title="Прочее" 
+                products={productOthers} 
+                isLoading={othersLoading} 
+                error={othersError} 
+            />
+        </>
+    )
+
 }
  
 export default MainPage;
