2629687e30e54fc4f43f805b42eaa95f515ede82
Andrii Shevchuk <supamercron@gmail.com>
2024-02-17 19:31:44 -0500

    fix problem with vite-plugin-svgr

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr({ exportAsDefault: true })],
+  plugins: [react(), svgr({ exportAsDefault: true, include: '**/*.svg' })],
 
   server: {
     open: true,
